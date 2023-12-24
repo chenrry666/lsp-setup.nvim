d7c16bb5eee7401c1754def2d6f9bcaa5a2c6080
chenrry666 <chengruichen3@gmail.com>
2023-12-24 13:23:46 +0800

    feat: use existing servers if possible

--- lua/lsp-setup/init.lua
+++ lua/lsp-setup/init.lua
@@ -16,29 +16,6 @@ local function lsp_servers(opts)
                 capabilities = opts.capabilities,
                 settings = {},
             })
-
-            local ok1, cmp = pcall(require, 'cmp_nvim_lsp')
-            if ok1 then
-                config.capabilities = vim.tbl_deep_extend(
-                    'keep',
-                    cmp.default_capabilities(),
-                    opts.capabilities
-                )
-            end
-            local ok2, coq = pcall(require, 'coq')
-            if ok2 then
-                config = coq.lsp_ensure_capabilities(config)
-            end
-            -- if opts.inlay_hints.enabled == true then
-            --     config.capabilities.textDocument = {
-            --         inlayHint = {
-            --             dynamicRegistration = false,
-            --             resolveSupport = {
-            --                 properties = {},
-            --             },
-            --         },
-            --     }
-            -- end
         end
 
         servers[server_name] = config
@@ -47,72 +24,51 @@ local function lsp_servers(opts)
     return servers
 end
 
-local default_mappings = {
-    gD = vim.lsp.buf.declaration,
-    gd = vim.lsp.buf.definition,
-    gi = vim.lsp.buf.implementation,
-    gr = vim.lsp.buf.references,
-    K = vim.lsp.buf.hover,
-    ['<C-k>'] = vim.lsp.buf.signature_help,
-    ['<space>rn'] = vim.lsp.buf.rename,
-    ['<space>ca'] = vim.lsp.buf.code_action,
-    ['<space>f'] = vim.lsp.buf.formatting,
-    ['<space>e'] = vim.diagnostic.open_float,
-    ['[d'] = vim.diagnostic.goto_prev,
-    [']d'] = vim.diagnostic.goto_next,
-}
-
 local M = {}
 
---- @class Options
-M.defaults = {
-    default_mappings = true,
+--- @class LspSetup.Options
+M.opts = {
     --- @type table<string, string|function>
     mappings = {},
     capabilities = vim.lsp.protocol.make_client_capabilities(),
     --- @diagnostic disable-next-line: unused-local
-    on_attach = function(client, bufnr)
-        utils.format_on_save(client)
-    end,
+    on_attach = function(client, bufnr) end,
     --- @type table<string, table|function>
     servers = {},
     inlay_hints = inlay_hints.opts,
 }
 
---- @param opts Options
-function M.setup(opts)
+--- @param user_opts LspSetup.Options
+function M.setup(user_opts)
     if vim.fn.has('nvim-0.8') ~= 1 then
         vim.notify_once('LSP setup requires Neovim 0.8.0+', vim.log.levels.ERROR)
         return
     end
 
-    opts = vim.tbl_deep_extend('keep', opts, M.defaults)
+    M.opts = vim.tbl_deep_extend('force', M.opts, user_opts)
 
     vim.api.nvim_create_augroup('LspSetup', {})
     vim.api.nvim_create_autocmd('LspAttach', {
         group = 'LspSetup',
         callback = function(args)
             local bufnr = args.buf
-            local mappings = opts.mappings
-            if opts.default_mappings then
-                mappings = vim.tbl_deep_extend('keep', mappings or {}, default_mappings)
-            end
+            local mappings = M.opts.mappings
             utils.mappings(bufnr, mappings)
-        end
+        end,
     })
 
-    inlay_hints.setup(opts.inlay_hints)
-    local servers = lsp_servers(opts)
+    inlay_hints.setup(M.opts.inlay_hints)
+    local servers = lsp_servers(M.opts)
 
     local ok1, mason = pcall(require, 'mason')
     local ok2, mason_lspconfig = pcall(require, 'mason-lspconfig')
     if ok1 and ok2 then
-        if vim.api.nvim_get_commands({})['Mason'] == nil then
+        if mason.has_setup then
             mason.setup()
         end
-        mason_lspconfig.setup({
-            ensure_installed = utils.get_keys(opts.servers),
-        })
+
+        mason_lspconfig.setup {}
+
         mason_lspconfig.setup_handlers({
             function(server_name)
                 local config = servers[server_name] or nil
@@ -120,8 +76,13 @@ function M.setup(opts)
                     return
                 end
                 require('lspconfig')[server_name].setup(config)
-            end
+            end,
         })
+
+        for server_name, config in pairs(servers) do
+            require('lspconfig')[server_name].setup(config)
+        end
+
         return
     else
         for server_name, config in pairs(servers) do
